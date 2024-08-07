c6e636bfb55f4eeeb1612c4b41a3983279ca7d81
Qasim <qalhamad33@gmail.com>
2024-08-07 12:06:53 +0300

    final

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
+import "./InventoryList.css";
 
 const InventoryList = (props) => {
+  function deriveButtonText() {
+    if (props.title.toLowerCase().includes("shop")) {
+      return "Add Item";
+    } else {
+      return "Remove Item";
+    }
+  }
   return (
-    <div>
+    <div className="inventory-list">
       <h2>{props.title}</h2>
       <ul>
         {props.inventory.map((item) => (
           <li key={item._id}>
             <p>{item.name}</p>
             <p>Price: {item.price}</p>
+            <button onClick={() => props.handleItemClick(item)}>
+              {deriveButtonText()}
+            </button>
           </li>
         ))}
       </ul>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./Shop.css";
-
 //Data
 import { inventoryData } from "../../data/data";
 
@@ -13,17 +12,41 @@ const Shop = () => {
   const [userInventory, setUserInventory] = useState([]);
 
   //Handlers
-  const handleAddItem = () => {
-    setShopInventory([]);
+  const handleAddItem = (selectedItem) => {
+    // filter item out of the inventory
+    const newShopInventory = shopInventory.filter(
+      (item) => item._id !== selectedItem._id
+    );
+    setShopInventory(newShopInventory);
+    setUserInventory([...userInventory, selectedItem]);
+
+    // add the incoming item to the user list
+  };
+
+  const handleRemoveItem = (selectedItem) => {
+    // filter item out of the inventory
+    const newUserInventory = userInventory.filter(
+      (item) => item._id !== selectedItem._id
+    );
+    setUserInventory(newUserInventory);
+    setShopInventory([...shopInventory, selectedItem]);
   };
   return (
     <main>
       <h1>Shop</h1>
       <section className="shop-section">
         {/* shop */}
-        <InventoryList title="Shop List" inventory={shopInventory} />
+        <InventoryList
+          title="Shop List"
+          inventory={shopInventory}
+          handleItemClick={handleAddItem}
+        />
         {/* cart */}
-        <InventoryList title="User List" inventory={userInventory} />
+        <InventoryList
+          title="User List"
+          inventory={userInventory}
+          handleItemClick={handleRemoveItem}
+        />
       </section>
     </main>
   );
